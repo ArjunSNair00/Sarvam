5d39ac288b697d2454b0013c6600405a839f6a30
ArjunSNair00 <snairarjun43@gmail.com>
2026-01-14 00:06:05 +0530

    some

--- app/(tabs)/Login.tsx
+++ app/(tabs)/Login.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 const Login = () => {
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <Text>Email: </Text>
     </View>
   );
 };
